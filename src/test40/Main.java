2634a9d56effb6c1c3f877600faca39142cda1b8
xuan1 <675773459@qq.com>
2018-04-04 13:54:27 +0800

    4.4

--- src/test40/Main.java
+++ src/test40/Main.java
@@ -3,10 +3,19 @@ package test40;
 import java.util.Scanner;
 
 public class Main {
+    public static int a =0;
+    public int b = 0;
+    public int aa(){
+        a++;
+        return a;
+    }
+    public int bb(){
+        b++;
+        return b;
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-
-        }
+        Main m = new Main();
+        System.out.println(m.bb());
+        System.out.println(m.aa());
     }
 }
